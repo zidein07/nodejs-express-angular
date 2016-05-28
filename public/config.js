a31a248115453f6dd76a14a9100d543cfb009c97
zidein <zidein07@gmail.com>
2016-05-29 00:23:18 +0300

    update setting with dir and style for dest

--- public/config.js
+++ public/config.js
@@ -9,6 +9,13 @@ app.disable('x-powered-by');
 app.set('view engine', 'jade');
 app.set('views', 'public/views');
 
+app.use(function(req, res, next) {
+    console.log('%c %s', req.method, req.url);
+    next()
+});
+
+app.use(express.static(__dirname + '/dist'));
+
 app.get('/', function(req, res) {
     res.render('main', data)
 });
