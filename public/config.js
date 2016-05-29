5a5c02854616645ddab3553449a27f130faa6c1b
zidein <zidein07@gmail.com>
2016-05-30 00:24:17 +0300

    update setting with server

--- public/config.js
+++ public/config.js
@@ -1,25 +1,40 @@
 var express = require('express');
 var app = express();
 
-var data = {
-    title: 'main page'    
+var pages = {
+    main: {
+        title: 'main page'
+    },
+    about: {
+        title: 'about page'
+    },
+    blog: {
+        title: 'blog page'
+    }
 };
 
 app.disable('x-powered-by');
 app.set('view engine', 'jade');
 app.set('views', 'public/views');
 
-app.use(function(req, res, next) {
-    console.log('%c %s', req.method, req.url);
+app.use(function (req, res, next) {
     next()
 });
 
 app.use(express.static(__dirname + '/dist'));
 
-app.get('/', function(req, res) {
-    res.render('main', data)
+app.get('/:page?', function (req, res) {
+    var currentPage = req.params.page, data;
+    if (!currentPage) currentPage = 'main';
+    data = pages[currentPage];
+    if (!data) {
+        res.redirect('/');
+        return;
+    }
+    console.log('currentPage', currentPage);
+    res.render(currentPage, data)
 });
 
-app.listen('80', function() {
+app.listen('80', function () {
     console.log('Im listen you');
 });
